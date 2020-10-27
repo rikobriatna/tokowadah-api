5e70591047be9a1210071b5a32df601dfc2ec9dd
Riko <rikobriatna01@gmail.com>
2020-10-27 17:13:42 +0700

    test html send email

--- application/controllers/Utils.php
+++ application/controllers/Utils.php
@@ -6,52 +6,19 @@ class Utils extends CI_Controller {
     // construct
     public function __construct(){
         parent::__construct();
+        $this->load->model('MTrans');
     }
 
     public function kirimEmail(){
+        $result['data']=$this->MTrans->getInvoice("2");
+        $msg = $this->load->view('notif/v_belum_bayar', $result);
         $from = "info@tokowadah.com";
         $to = "rikobriatna01@gmail.com";
         $subject = "Checking PHP mail";
-        $message = "PHP mail berjalan dengan baik";
+        $message = $msg;
         $headers = "From:" . $from;
         mail($to,$subject,$message, $headers);
         echo "Pesan email sudah terkirim.";
+
     }
-
-    public function send_mail() {
-
-        $from_email = "info@tokowadah.com";
-        $to_email = "rikobriatna01@gmail.com";
-        $user = "info@tokowadah.com";
-        $pass = "tokowadah01";
-
-        $config = Array(
-            'protocol' => 'smtp',
-            'smtp_host' => 'mail.tokowadah.com',
-            'smtp_port' => 465,
-            'smtp_user' => $user,
-            'smtp_pass' => $pass,
-            'mailtype'  => 'html',
-            'charset'   => 'iso-8859-1'
-        );
-
-        $this->load->library('email', $config);
-        $this->email->set_newline("\r\n");
-
-        $this->email->from($from_email, "Info TokoWadah");
-         $this->email->to($to_email);
-         $this->email->subject('Test Pengiriman Email');
-         $this->email->message("Coba mengirim Email dengan CodeIgniter.");
-
-         //Send mail
-         if($this->email->send()){
-//             $this->session->set_flashdata("notif","Email berhasil terkirim.");
-             echo "Email berhasil terkirim.";
-         }else {
-//             $this->session->set_flashdata("notif","Email gagal dikirim.");
-             echo "Email gagal dikirim.";
-             echo "<br>";
-             echo $this->email->print_debugger();
-         }
-      }
 }
