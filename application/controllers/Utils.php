ebdbf3cad77ddbf3748f9ba0539f0526368cd9b7
Riko <rikobriatna01@gmail.com>
2020-11-01 15:34:41 +0700

    email test view

--- application/controllers/Utils.php
+++ application/controllers/Utils.php
@@ -15,19 +15,19 @@ class Utils extends CI_Controller
     {
         $result['data'] = $this->MTrans->getInvoice("2");
         $this->load->view('notif/v_test');
-//        $msg = $this->load->view('notif/v_belum_bayar', $result, TRUE);
-//        $from = "info@tokowadah.com";
-//        $to = "rikobriatna01@gmail.com";
-//        $subject = "Checking PHP mail";
-//        $message = $msg;
-////        $headers = "From:" . $from;
-//        // Always set content-type when sending HTML email
-//        $headers = "MIME-Version: 1.0" . "\r\n";
-//        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-//// More headers
-//        $headers .= 'From: <info@tokowadah.com>' . "\r\n";
-//        mail($to, $subject, $message, $headers);
-//        echo "Pesan email sudah terkirim.";
+        $msg = $this->load->view('notif/v_belum_bayar', $result, TRUE);
+        $from = "info@tokowadah.com";
+        $to = "rikobriatna01@gmail.com";
+        $subject = "Checking PHP mail";
+        $message = $msg;
+//        $headers = "From:" . $from;
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+// More headers
+        $headers .= 'From: <info@tokowadah.com>' . "\r\n";
+        mail($to, $subject, $message, $headers);
+        echo "Pesan email sudah terkirim.";
 
     }
 }
