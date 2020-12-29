38680793b7f8aeb0d2e19b49a5173750df937787
Riko <rikobriatna01@gmail.com>
2020-12-29 19:59:44 +0700

    test layout email1

--- application/views/notif/v_test_bck.php
+++ application/views/notif/v_test_bck.php
@@ -395,11 +395,51 @@
                                          style="width: 100px; max-width: 600px; height: auto; margin-bottom: 20px; display: block;">
                                     <div class="text">
                                         <h3>Diesel Engine Kubota D722</h3>
+                                        <span>S, M, L, XL</span>
+                                        <p>A small river named Duden flows by their place and supplies</p>
                                     </div>
                                 </div>
                             </td>
                             <td valign="middle" width="30%" style="text-align:center; padding-right: 2.5em;">
-                                <span class="price" style="color: #f34949; font-size: 20px; display: block;">Rp. 1000</span>
+                                <span class="price" style="color: #f34949; font-size: 20px; display: block;">$120</span>
+                                <span style="display: block;">50% Discount</span>
+                                <span><a href="#" class="btn btn-primary">Shop now</a></span>
+                            </td>
+                        </tr>
+                        <tr style="border-bottom: 1px solid rgba(0,0,0,.05);">
+                            <td valign="middle" width="70%" style="text-align:left; padding: 0 2.5em;">
+                                <div class="product-entry">
+                                    <img src="images/product-2.jpg" alt=""
+                                         style="width: 100px; max-width: 600px; height: auto; margin-bottom: 20px; display: block;">
+                                    <div class="text">
+                                        <h3>Young Woman Dress</h3>
+                                        <span>S, M, L, XL</span>
+                                        <p>A small river named Duden flows by their place and supplies</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td valign="middle" width="30%" style="text-align:center; padding-right: 2.5em;">
+                                <span class="price" style="color: #f34949; font-size: 20px; display: block;">$120</span>
+                                <span style="display: block;">50% Discount</span>
+                                <span><a href="#" class="btn btn-primary">Shop now</a></span>
+                            </td>
+                        </tr>
+                        <tr style="border-bottom: 1px solid rgba(0,0,0,.05);">
+                            <td valign="middle" width="70%" style="text-align:left; padding: 0 2.5em;">
+                                <div class="product-entry">
+                                    <img src="images/product-3.jpg" alt=""
+                                         style="width: 100px; max-width: 600px; height: auto; margin-bottom: 20px; display: block;">
+                                    <div class="text">
+                                        <h3>Young Woman Dress</h3>
+                                        <span>S, M, L, XL</span>
+                                        <p>A small river named Duden flows by their place and supplies</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td valign="middle" width="30%" style="text-align:center; padding-right: 2.5em;">
+                                <span class="price" style="color: #f34949; font-size: 20px; display: block;">$120</span>
+                                <span style="display: block;">50% Discount</span>
+                                <span><a href="#" class="btn btn-primary">Shop now</a></span>
                             </td>
                         </tr>
                     </table>
@@ -413,26 +453,41 @@
                 <td valign="middle" class="bg_light footer email-section">
                     <table>
                         <tr>
-                            <td valign="top" width="50%" style="padding-top: 20px;">
+                            <td valign="top" width="33.333%" style="padding-top: 20px;">
                                 <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
                                     <tr>
                                         <td style="text-align: left; padding-right: 10px;">
-                                            <h3 class="heading">Tentang Kita</h3>
-                                            <p>Tokowadah merupakan situs bisnis online B2B alat industrial untuk pengadaan dan penjualan material atau suku cadang yang berkonsentrasi saat ini pada bidang pengeboran dan konstruksi</p>
+                                            <h3 class="heading">About</h3>
+                                            <p>A small river named Duden flows by their place and supplies it with the
+                                                necessary regelialia.</p>
                                         </td>
                                     </tr>
                                 </table>
                             </td>
-                            <td valign="top" width="50%" style="padding-top: 20px;">
+                            <td valign="top" width="33.333%" style="padding-top: 20px;">
                                 <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
                                     <tr>
                                         <td style="text-align: left; padding-left: 5px; padding-right: 5px;">
-                                            <h3 class="heading">Info Kontak</h3>
+                                            <h3 class="heading">Contact Info</h3>
                                             <ul>
-                                                <li><span class="text">Jl. Pesona Anggrek, Blok F6/9, Harapan Jaya, Bekasi Utara, Indonesia</span>
+                                                <li><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span>
                                                 </li>
-                                                <li><span class="text">+6281296591638</span></a></li>
-                                                <li><span class="text">info@tokowadah.com</span></a></li>
+                                                <li><span class="text">+2 392 3929 210</span></a></li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                            <td valign="top" width="33.333%" style="padding-top: 20px;">
+                                <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                                    <tr>
+                                        <td style="text-align: left; padding-left: 10px;">
+                                            <h3 class="heading">Useful Links</h3>
+                                            <ul>
+                                                <li><a href="#">Home</a></li>
+                                                <li><a href="#">Account</a></li>
+                                                <li><a href="#">Wishlist</a></li>
+                                                <li><a href="#">Order</a></li>
                                             </ul>
                                         </td>
                                     </tr>
