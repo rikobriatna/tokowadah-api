67717eacaa42f23b0ae93a3acece1bb1ce0167b4
Riko <rikobriatna01@gmail.com>
2020-12-29 20:12:46 +0700

    test layout email1 kirim

--- application/controllers/Utils.php
+++ application/controllers/Utils.php
@@ -15,7 +15,7 @@ class Utils extends CI_Controller
     {
 //        $result['data'] = $this->MTrans->getInvoice("2");
 //        $this->load->view('notif/v_test');
-        $msg = $this->load->view('notif/v_test', null);
+        $msg = $this->load->view('notif/v_test', null, true);
         $from = "info@tokowadah.com";
         $to = "rikobriatna01@gmail.com";
         $subject = "Checking PHP mail";
